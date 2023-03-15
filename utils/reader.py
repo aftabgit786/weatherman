fc8959f63fdb7066a84d16a02dcebe39cc78c087
aftabgit786 <gb485585@gmail.com>
2023-03-15 13:14:42 +0500

    t2

--- utils/reader.py
+++ utils/reader.py
@@ -1,5 +1,13 @@
 import os
 
+def get_contents_for_events(file_content):
+    file_values = set()
+    for row in file_content.split("\n")[1:-1]:
+        values = row.split(",")
+        file_values.add(values[-2])
+    return file_values
+
+
 def get_contents_of_one_file(file_content):
     file_values = []
     for row in file_content.split("\n")[1:-1]:
@@ -21,6 +29,20 @@ def read_files():
             file_content = file_reader.read()
 
             file_values += get_contents_of_one_file(file_content)
-            file_reader.close()
 
     return file_values
+
+def read_files_for_events():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    weatherfiles_path = os.path.join(dir_path, '..', 'weatherfiles')
+
+    file_names = os.listdir(weatherfiles_path)
+
+    file_values = set()
+    for file_name in file_names:
+        with open(os.path.join(weatherfiles_path, file_name), "r") as file_reader:
+            file_content = file_reader.read()
+            file_values |= get_contents_for_events(file_content)
+            file_reader.close()
+    return file_values
+
