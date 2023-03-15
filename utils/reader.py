6b77f0cfa42c5415a978d8b88abc6a59f9fbfb50
aftabgit786 <gb485585@gmail.com>
2023-03-15 10:45:58 +0500

    update my project

--- utils/reader.py
+++ utils/reader.py
@@ -1,5 +1,13 @@
 import os
 
+def get_contents_of_one_file(file_content):
+    file_values = []
+    for row in file_content.split("\n")[1:-1]:
+        file_values.append(row.split(","))
+
+    return file_values
+
+
 def read_files():
     dir_path = os.path.dirname(os.path.realpath("weatherman.weatherfiles/"))
     weatherfiles_path = os.path.join(dir_path, '..', 'weatherfiles')
@@ -11,7 +19,6 @@ def read_files():
     for file_name in file_names:
         with open(os.path.join(weatherfiles_path, file_name), "r") as file_reader:
             file_content = file_reader.read()
-            from weatherman.utils.content import get_contents_of_one_file
 
             file_values += get_contents_of_one_file(file_content)
             file_reader.close()
